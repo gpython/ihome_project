9c7042452a8e099d11d270a055fcaf6ba51c14c4
gpython <157650138@qq.com>
2018-11-27 12:03:28 +0800

    first commit

--- apps/notes/flask.vim
+++ apps/notes/flask.vim
@@ -2,3 +2,16 @@ python manage.py db init
 python manage.py db migrate -m "init tables"  #初始化 需要有文件使用models
 python manage.py db upgrade
 python manage.py db downgrade
+
+
+Post请求 服务器csrf
+csrf验证机制
+cookie:
+  csrf_token = xxx
+body:
+  csrf_token = xxx
+从cookie中获取一个csrf_token值
+从请求体中获取一个csrf_token值
+如果两个值相等 则检验通过
+可以进入视图函数中检验
+如果两个值不同 校验失败 想前端返回400状态码
--- apps/web_html.py
+++ apps/web_html.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, make_response
+from flask_wtf import csrf
+
 
 #提供静态文件的蓝图
 html = Blueprint("web_html", __name__)
-
+print('.......')
 #http://127.0.0.1/()
 #http://127.0.0.1/(index.html)
 #http://127.0.0.1/(register.html)
@@ -25,5 +27,15 @@ def get_html(html_file_name):
   if html_file_name != "favicon.ico":
     html_file_name = "html/" + html_file_name
 
-  #flask提供返回静态文件的方法
-  return current_app.send_static_file(html_file_name)
+  #创建一个csrf_token
+  csrf_token = csrf.generate_csrf()
+
+  # flask提供返回静态文件的方法
+  resp = make_response(current_app.send_static_file(html_file_name))
+
+  #设置cookie值
+  resp.set_cookie("csrf_token", csrf_token)
+  print('-------')
+  return resp
+
+
